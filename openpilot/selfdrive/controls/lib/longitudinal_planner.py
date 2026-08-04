5081945228a2a8958067727c7368fbb080f8bfb9
Erik Fleischer <erik@erlenweg.de>
2026-08-04 11:14:43 +0200

    mpc long: limit speed based on predicted curvature

--- openpilot/selfdrive/controls/lib/longitudinal_planner.py
+++ openpilot/selfdrive/controls/lib/longitudinal_planner.py
@@ -130,7 +130,8 @@ class LongitudinalPlanner:
 
     self.mpc.set_weights(prev_accel_constraint, personality=sm['selfdriveState'].personality)
     self.mpc.set_cur_state(self.v_desired_filter.x, self.a_desired)
-    self.mpc.update(v_cruise, sm['modelV2'], sm['radarState'], personality=sm['selfdriveState'].personality)
+    self.mpc.update(v_cruise, sm['modelV2'], sm['radarState'], sm['drivingModelData'].path,
+                    personality=sm['selfdriveState'].personality)
 
     self.v_desired_trajectory = np.interp(CONTROL_N_T_IDX, T_IDXS_MPC, self.mpc.v_solution)
     self.a_desired_trajectory = np.interp(CONTROL_N_T_IDX, T_IDXS_MPC, self.mpc.a_solution)
